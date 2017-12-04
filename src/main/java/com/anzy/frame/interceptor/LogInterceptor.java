5a59b56d07f805c6f2c8cf00a853259ed0514317
SunSmileAZY <1157451283@qq.com>
2017-12-04 10:31:02 +0800

    修改AOP日志问题， 原因：扫包重复造成AOP失效

--- src/main/java/com/anzy/frame/interceptor/LogInterceptor.java
+++ src/main/java/com/anzy/frame/interceptor/LogInterceptor.java
@@ -15,7 +15,7 @@ import org.springframework.core.Ordered;
 
 import java.util.ArrayList;
 
-@Aspect
+//@Aspect
 public class LogInterceptor implements Ordered {
 
     private int order;
@@ -27,14 +27,15 @@ public class LogInterceptor implements Ordered {
     }
     private final Logger logger = LoggerFactory.getLogger(LogInterceptor.class);
 
-    @Before("execution (* com.anzy.bussiness.sys.*.*.*.*(..))")
+//    @Before("execution (* com.anzy.bussiness.sys.*.*.*.*(..))")
     public void doBefore(JoinPoint jp){
+//        System.out.print("进入dobefore===============");
         if(logger.isInfoEnabled()){
             String strLog =  jp.getTarget().getClass().getName() + "." + jp.getSignature().getName();
             ArrayList<Object> aList = new ArrayList<Object>();
 
             for(Object obj : jp.getArgs()){
-                if(obj != null && (obj.getClass().getName().startsWith("java.lang") || obj.getClass().getName().startsWith("com.airtrip"))){
+                if(obj != null && (obj.getClass().getName().startsWith("java.lang") || obj.getClass().getName().startsWith("com.anzy"))){
                     aList.add(obj);
                 }
             }
@@ -43,7 +44,7 @@ public class LogInterceptor implements Ordered {
         }
     }
 
-    @AfterReturning(value = "execution (* com.anzy.bussiness.sys.service.impl.*.*(..))",  returning="rtv")
+//    @AfterReturning(value = "execution (* com.anzy.bussiness.sys.service.impl.*.*(..))",  returning="rtv")
     private void doAfterReturning(JoinPoint jp, Object rtv){
         if(logger.isInfoEnabled()){
             String strLog =  jp.getTarget().getClass().getName() + "." + jp.getSignature().getName();
